c0fceb47e1ee719678562b8829a170503a9e7fc2
felipoxy1 <luizfelipepassos2015@gmail.com>
2025-01-29 14:29:59 -0300

    Fazendo correcao no dever

--- funcionarios.js
+++ funcionarios.js
@@ -33,12 +33,33 @@ function Funcionario(nome, cargo, salario){
     }
 }
 
+function Cliente(nome, carro) {
+    Pessoa.call(this, nome)
+
+    let _carro = carro
+
+    this.getCarro = function(){
+        return _carro
+    }
+}
+
 const funcionario1 = new Funcionario("Orlando", "Gerente", 5000)
 const funcionario2 = new Funcionario("Davi", "Lavador", 1600)
+const funcionario3 = new Funcionario("Pedro","Lavador", 1600)
+
+const cliente1 = new Cliente("Ruan", "HB20")
+const cliente2 = new Cliente("Orlando", "Palio")
+const cliente3 = new Cliente("Luiz", "Argo")
 
 funcionario2.setSalario(2500)
 funcionario2.setCargo("sub-gerente")
 
 console.log(funcionario1.nome, funcionario1.getCargo() ,funcionario1.getSalario())
 console.log(funcionario2.nome, funcionario2.getCargo() ,funcionario2.getSalario())
+console.log(funcionario3.nome, funcionario3.getCargo() ,funcionario3.getSalario())
+
+console.log(cliente1.nome, cliente1.getCarro())
+console.log(cliente2.nome, cliente2.getCarro())
+console.log(cliente3.nome, cliente3.getCarro())
+
 
