23b498ba01e7726ed32523edb26c2064c033fa98
felipoxy1 <luizfelipepassos2015@gmail.com>
2025-03-07 19:04:12 -0300

    Introduzindo o dever feito

---
+++ src/dever.js
@@ -0,0 +1,9 @@
+"use strict";
+function dizOla(nome) {
+    return console.log(`Ola ${nome}`);
+}
+dizOla('Luiz');
+function Multiplica(numero1, numero2) {
+    return numero1 * numero2;
+}
+console.log(`O resultado do seu calculo foi`, Multiplica(9, 32));
---
+++ src/dever.ts
@@ -0,0 +1,11 @@
+function dizOla(nome: string) {
+    return console.log(`Ola ${nome}`)
+}
+
+dizOla('Luiz')
+
+function Multiplica(numero1: number, numero2: number):number {
+    return numero1 * numero2
+}
+
+console.log(`O resultado do seu calculo foi`, Multiplica(9, 32))
